4224e2514f12cde46ace6840886797510d196c38
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-07 09:28:47 -0600

    Add seed data (up) and reverse (down)

--- seeders/20190807152056-game_seed.js
+++ seeders/20190807152056-game_seed.js
@@ -12,6 +12,40 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    return queryInterface.bulkInsert('Games', [
+        {
+          title: 'Fix it Felix Jr.',
+          price: 50,
+          releaseYear: 1982,
+          active: true
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Ms. Pac Man',
+          price: 100,
+          releaseYear: 1981,
+          active: true
+        createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Dig Dug',
+          price: 75,
+          releaseYear: 1982,
+          active: false
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Galaga',
+        price: 125,
+        releaseYear: 1981,
+        active: true
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +56,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
+    return queryInterface.bulkDelete('Games', null, {});
   }
 };
