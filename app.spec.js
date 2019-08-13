a266b99fe13ab8383929ed29a26bb0f11aac5b85
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-13 14:07:46 -0600

    Add spec file

---
+++ app.spec.js
@@ -0,0 +1,27 @@
+var shell = require('shelljs');
+var request = require('supertest');
+var app = require('./app');
+
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create');
+  });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate');
+    shell.exec('npx sequelize db:seed:all');
+  });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all');
+  });
+
+  describe('Test the root path', () => {
+    test('should return 200', () => {
+      return request(app).get('/')
+        .then(
+          response =>{
+            expect(response.statusCode).toBe(200)
+          }
+        )
+    })
+  })
+})
